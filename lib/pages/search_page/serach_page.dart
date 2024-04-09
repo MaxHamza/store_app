39dbe020626b2d6147fc6377430ba870331fe73e
Hamza <hamza1mks1@gmail.com>
2024-04-09 18:53:05 +0300

    fix search error end time2

--- lib/pages/search_page/serach_page.dart
+++ lib/pages/search_page/serach_page.dart
@@ -28,7 +28,7 @@ class SearchPage extends StatelessWidget {
                 ? Expanded(
                     child: ListView.builder(
                       physics:const BouncingScrollPhysics(),
-                        itemCount: controller.visibleProducts.length<=6?controller.visibleProducts.length:6,
+                        itemCount: controller.visibleProducts.length,
                         itemBuilder: (context, index) {
                           return GestureDetector(
                             onTap: (){
